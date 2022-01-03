55b70faf1cc4a5fbb9fc35755ca50273dba059f5
Kaeli <kaelif@alumni.ubc.ca>
2022-01-02 16:08:53 -0800

    Cleaned out unused code

--- api/routes/testAPI.js
+++
@@ -1,8 +0,0 @@
-// var express = require('express');
-// var router = express.Router();
-
-// router.get('/', (req, res) => {
-//     res.set('Access-Control-Allow-Origin', '*');
-//     res.send({ 'msg': 'API is working 1' })
-// })
-// module.exports = router;
--- server/config/db.js
+++ server/config/db.js
@@ -1,3 +1,5 @@
+const express = require('express');
+const app = express();
 var Connection = require('tedious').Connection;
 
 var config = {
@@ -28,7 +30,7 @@ connection.connect();
 var Request = require('tedious').Request;
 
 function executeStatement() {
-    request = new Request("select * from SalesLT.Customer", function (err, rowCount) {
+    request = new Request("SELECT  c.LastName + ', ' + c.FirstName AS [Name],	c.Title + ' ' + c.FirstName + ' ' + c.LastName AS FullName, c.EmailAddress AS Email, c.Phone FROM SalesLT.Customer c WHERE c.FirstName IS NOT NULL AND c.LastName IS NOT NULL AND c.EmailAddress IS NOT NULL AND c.Phone IS NOT NULL ORDER BY c.LastName, c.FirstName", function (err, rowCount) {
         if (err) {
             throw err;
         } else {
@@ -36,11 +38,20 @@ function executeStatement() {
         }
     });
 
+    var customers = [];
+
     request.on('row', function (columns) {
         columns.forEach(function (column) {
-            console.log(column.value);
+            customers.push(column.value);
         });
     });
 
     connection.execSql(request);
+
+    app.get('/', (req, res) => {
+        res.set('Access-Control-Allow-Origin', '*');
+        res.send(customers)
+    })
 }
+
+module.exports = app;
